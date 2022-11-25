0b2def2fe15922bee66324789cccc3c69204287d
MURAKI Taichi <muraki.taichi@gmail.com>
2022-11-25 16:49:27 +0900

    fix

--- src/modeling/_gcnn.py
+++ src/modeling/_gcnn.py
@@ -1,14 +1,18 @@
 from __future__ import division
-import torch
-import torch.nn.functional as F
+
+import math
+
 import numpy as np
 import scipy.sparse
-import math
+import torch
+import torch.nn.functional as F
+
 
 class SparseMM(torch.autograd.Function):
     """Redefine sparse @ dense matrix multiplication to enable backpropagation.
     The builtin matrix multiplication operation does not support backpropagation in some cases.
     """
+
     @staticmethod
     def forward(ctx, sparse, dense):
         ctx.req_grad = dense.requires_grad
@@ -16,17 +20,13 @@ class SparseMM(torch.autograd.Function):
         return torch.matmul(sparse, dense)
 
     @staticmethod
-    def symbolic(g: torch._C.Graph, input: torch._C.Value) -> torch._C.Value:
-        #return g.op("Clip", input, g.op("Constant", value_t=torch.tensor(0, dtype=torch.float)))
-        return g.op("MatMul", input)
-
-    @staticmethod
     def backward(ctx, grad_output):
         grad_input = None
-        sparse, = ctx.saved_tensors
+        (sparse,) = ctx.saved_tensors
         if ctx.req_grad:
             grad_input = torch.matmul(sparse.t(), grad_output)
         return None, grad_input
+
 
 def spmm(sparse, dense):
     return SparseMM.apply(sparse, dense)
@@ -34,16 +34,16 @@ def spmm(sparse, dense):
 
 def gelu(x):
     """Implementation of the gelu activation function.
-        For information: OpenAI GPT's gelu is slightly different (and gives slightly different results):
-        0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
-        Also see https://arxiv.org/abs/1606.08415
+    For information: OpenAI GPT's gelu is slightly different (and gives slightly different results):
+    0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+    Also see https://arxiv.org/abs/1606.08415
     """
     return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
 
+
 class BertLayerNorm(torch.nn.Module):
     def __init__(self, hidden_size, eps=1e-12):
-        """Construct a layernorm module in the TF style (epsilon inside the square root).
-        """
+        """Construct a layernorm module in the TF style (epsilon inside the square root)."""
         super(BertLayerNorm, self).__init__()
         self.weight = torch.nn.Parameter(torch.ones(hidden_size))
         self.bias = torch.nn.Parameter(torch.zeros(hidden_size))
@@ -60,7 +60,8 @@ class GraphResBlock(torch.nn.Module):
     """
     Graph Residual Block similar to the Bottleneck Residual Block in ResNet
     """
-    def __init__(self, in_channels, out_channels, mesh_type='body'):
+
+    def __init__(self, in_channels, out_channels, mesh_type="body"):
         super(GraphResBlock, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -74,18 +75,19 @@ class GraphResBlock(torch.nn.Module):
         self.norm2 = BertLayerNorm(out_channels // 2)
 
     def forward(self, x):
-        trans_y = F.relu(self.pre_norm(x)).transpose(1,2)
-        y = self.lin1(trans_y).transpose(1,2)
+        trans_y = F.relu(self.pre_norm(x)).transpose(1, 2)
+        y = self.lin1(trans_y).transpose(1, 2)
 
         y = F.relu(self.norm1(y))
         y = self.conv(y)
 
-        trans_y = F.relu(self.norm2(y)).transpose(1,2)
-        y = self.lin2(trans_y).transpose(1,2)
+        trans_y = F.relu(self.norm2(y)).transpose(1, 2)
+        y = self.lin2(trans_y).transpose(1, 2)
 
-        z = x+y
+        z = x + y
 
         return z
+
 
 # class GraphResBlock(torch.nn.Module):
 #     """
@@ -105,10 +107,12 @@ class GraphResBlock(torch.nn.Module):
 #         z = x+y
 #         return z
 
+
 class GraphLinear(torch.nn.Module):
     """
     Generalization of 1x1 convolutions on Graphs
     """
+
     def __init__(self, in_channels, out_channels):
         super(GraphLinear, self).__init__()
         self.in_channels = in_channels
@@ -125,35 +129,37 @@ class GraphLinear(torch.nn.Module):
     def forward(self, x):
         return torch.matmul(self.W[None, :], x) + self.b[None, :, None]
 
+
 class GraphConvolution(torch.nn.Module):
     """Simple GCN layer, similar to https://arxiv.org/abs/1609.02907."""
-    def __init__(self, in_features, out_features, mesh='body', bias=True):
+
+    def __init__(self, in_features, out_features, mesh="body", bias=True):
         super(GraphConvolution, self).__init__()
-        device=torch.device('cpu')
+        device = torch.device("cuda")
         self.in_features = in_features
         self.out_features = out_features
 
-        if mesh=='body':
-            adj_indices = torch.load('./src/modeling/data/smpl_431_adjmat_indices.pt')
-            adj_mat_value = torch.load('./src/modeling/data/smpl_431_adjmat_values.pt')
-            adj_mat_size = torch.load('./src/modeling/data/smpl_431_adjmat_size.pt')
-        elif mesh=='hand':
-            adj_indices = torch.load('./src/modeling/data/mano_195_adjmat_indices.pt')
-            adj_mat_value = torch.load('./src/modeling/data/mano_195_adjmat_values.pt')
-            adj_mat_size = torch.load('./src/modeling/data/mano_195_adjmat_size.pt')
+        if mesh == "body":
+            adj_indices = torch.load("./src/modeling/data/smpl_431_adjmat_indices.pt")
+            adj_mat_value = torch.load("./src/modeling/data/smpl_431_adjmat_values.pt")
+            adj_mat_size = torch.load("./src/modeling/data/smpl_431_adjmat_size.pt")
+        elif mesh == "hand":
+            adj_indices = torch.load("./src/modeling/data/mano_195_adjmat_indices.pt")
+            adj_mat_value = torch.load("./src/modeling/data/mano_195_adjmat_values.pt")
+            adj_mat_size = torch.load("./src/modeling/data/mano_195_adjmat_size.pt")
 
-        self.adjmat = torch.sparse_coo_tensor(adj_indices, adj_mat_value, size=adj_mat_size).to_dense()
+        self.adjmat = torch.sparse_coo_tensor(adj_indices, adj_mat_value, size=adj_mat_size).to(device)
 
         self.weight = torch.nn.Parameter(torch.FloatTensor(in_features, out_features))
         if bias:
             self.bias = torch.nn.Parameter(torch.FloatTensor(out_features))
         else:
-            self.register_parameter('bias', None)
+            self.register_parameter("bias", None)
         self.reset_parameters()
 
     def reset_parameters(self):
         # stdv = 1. / math.sqrt(self.weight.size(1))
-        stdv = 6. / math.sqrt(self.weight.size(0) + self.weight.size(1))
+        stdv = 6.0 / math.sqrt(self.weight.size(0) + self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
@@ -169,14 +175,12 @@ class GraphConvolution(torch.nn.Module):
             output = []
             for i in range(x.shape[0]):
                 support = torch.matmul(x[i], self.weight)
-                output.append(torch.matmul(self.adjmat, support))
-                # output.append(spmm(self.adjmat, support))
+                # output.append(torch.matmul(self.adjmat, support))
+                output.append(spmm(self.adjmat, support))
             output = torch.stack(output, dim=0)
             if self.bias is not None:
                 output = output + self.bias
             return output
 
     def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
+        return self.__class__.__name__ + " (" + str(self.in_features) + " -> " + str(self.out_features) + ")"
