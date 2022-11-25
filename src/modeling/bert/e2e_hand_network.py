50392a6974af24dab8a56bc3cdd076b3eb8b2aaa
MURAKI Taichi <muraki.taichi@gmail.com>
2022-11-25 16:13:58 +0900

    fix

--- src/modeling/bert/e2e_hand_network.py
+++ src/modeling/bert/e2e_hand_network.py
@@ -49,10 +49,23 @@ class Graphormer_Hand_Network(torch.nn.Module):
         self.cam_param_fc3 = torch.nn.Linear(150, 3)
         self.grid_feat_dim = torch.nn.Linear(1024, 2051)
 
-    def forward(
-        self, images, template_vertices, template_3d_joints, template_vertices_sub, meta_masks=None, is_train=False
-    ):
+    def forward(self, images, mesh_model, mesh_sampler, meta_masks=None, is_train=False):
         batch_size = images.size(0)
+        # Generate T-pose template mesh
+        template_pose = torch.zeros((1, 48))
+        template_pose = template_pose.to(self.device)
+        template_betas = torch.zeros((1, 10)).to(self.device)
+        template_vertices, template_3d_joints = mesh_model.layer(template_pose, template_betas)
+        template_vertices = template_vertices / 1000.0
+        template_3d_joints = template_3d_joints / 1000.0
+
+        template_vertices_sub = mesh_sampler.downsample(template_vertices)
+
+        # normalize
+        template_root = template_3d_joints[:, cfg.J_NAME.index("Wrist"), :]
+        template_3d_joints = template_3d_joints - template_root[:, None, :]
+        template_vertices = template_vertices - template_root[:, None, :]
+        template_vertices_sub = template_vertices_sub - template_root[:, None, :]
         num_joints = template_3d_joints.shape[1]
         # concatinate template joints and template vertices, and then duplicate to batch size
         ref_vertices = torch.cat([template_3d_joints, template_vertices_sub], dim=1)
