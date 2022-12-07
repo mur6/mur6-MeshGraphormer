b7a77684990d5e460c0b067acf0f48372f21e70b
MURAKI Taichi <muraki.taichi@gmail.com>
2022-12-07 13:08:14 +0900

    fix

--- dataset_test_for_blender.py
+++ dataset_test_for_blender.py
@@ -1,35 +1,12 @@
-"""
-Copyright (c) Microsoft Corporation.
-Licensed under the MIT license.
-
-Training and evaluation codes for 3D hand mesh reconstruction from an image
-"""
-
-from __future__ import absolute_import, division, print_function
-
 import argparse
-import base64
-import code
-import datetime
-import gc
-import json
 import math
-import os
 import os.path as op
-import pickle
-import sys
 from collections import defaultdict
 from logging import DEBUG, INFO, basicConfig, critical, debug, error, exception, getLogger, info
 from os.path import join
 
-import cv2
-import imageio
 import matplotlib.pyplot as plt
 import numpy as np
-
-# from metro.modeling._mano import MANO
-# mano_mesh_model = MANO() # init MANO
-import scipy.misc
 import torch
 import torchvision.models as models
 import transforms3d
@@ -49,14 +26,6 @@ from src.modeling.hrnet.config import config as hrnet_config
 from src.modeling.hrnet.config import update_config as hrnet_update_config
 from src.modeling.hrnet.hrnet_cls_net_gridfeat import get_cls_net_gridfeat
 from src.utils.comm import all_gather, get_rank, get_world_size, is_main_process, synchronize
-from src.utils.geometric_layers import orthographic_projection
-from src.utils.image_ops import img_from_base64
-from src.utils.logger import setup_logger
-from src.utils.metric_logger import AverageMeter
-from src.utils.metric_pampjpe import reconstruction_error
-from src.utils.miscellaneous import mkdir, set_seed
-from src.utils.tsv_file import TSVFile
-from src.utils.tsv_file_ops import generate_hw_file, generate_linelist_file, tsv_reader, tsv_writer
 
 
 def build_hand_dataset(yaml_file, args, is_train=True, scale_factor=1):
