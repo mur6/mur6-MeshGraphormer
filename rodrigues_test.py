1756f2548c0e1801623a6ca4a2e621f3ad5bcc97
MURAKI Taichi <muraki.taichi@gmail.com>
2022-12-15 14:18:44 +0900

    fix

--- rodrigues_test.py
+++ rodrigues_test.py
@@ -1,5 +1,5 @@
 import torch
-
+import numpy as np
 from manopth import rodrigues_layer
 from scipy.spatial.transform import Rotation as R
 
@@ -38,13 +38,53 @@ def test_scipy_rodrigues_1():
 
 
 def test_scipy_rodrigues_2():
-    r = R.from_mrp([1.0667, -0.6252,  1.8175])
+    r = R.from_mrp([ 0.7129,  0.7157, -0.7929])
     print("euler:", r.as_euler('xyz', degrees=True))
     print("rodrigues:", r.as_mrp())
     # print(r.as_quat())
     print(r.as_matrix())
+    print("############")
+    a = torch.FloatTensor([[0.7129,  0.7157, -0.7929]])
+    rot_mats = rodrigues_layer.batch_rodrigues(a)
+    print(rot_mats)
 
 
-test_scipy_rodrigues_2()
-print("############")
-#main()
+def test_scipy_rodrigues_3():
+    r = R.from_mrp([ 0.7129,  0.7157, -0.7929])
+    print("euler:", r.as_euler('xyz', degrees=True))
+    print("rodrigues:", r.as_mrp())
+    # print(r.as_quat())
+    print(r.as_matrix())
+    print("############")
+    a = torch.FloatTensor([[0.7129,  0.7157, -0.7929]])
+    rot_mats = rodrigues_layer.batch_rodrigues(a)
+    print(rot_mats)
+
+def test_scipy_rodrigues_4():
+    a = np.array([ 0.5038,  0.8142,  0.2887, -0.3703,  0.5055, -0.7793, -0.7804,  0.2857, 0.5562])
+    a = a.reshape((3, 3))
+    r = R.from_matrix(a)
+    print(r.as_mrp())
+
+
+def test_scipy_rodrigues_5():
+    axisang = torch.FloatTensor([[ 0.7129,  0.7157, -0.7929]])
+    axisang_norm = torch.norm(axisang + 1e-8, p=2, dim=1)
+    angle = torch.unsqueeze(axisang_norm, -1)
+    print(axisang_norm, angle)
+    axisang_normalized = torch.div(axisang, angle)
+    # print(axisang_normalized)
+
+    # a = torch.FloatTensor([[0.7129,  0.7157, -0.7929]])
+    axisang = torch.FloatTensor([[ 0.7129,  0.7157, -0.7929]])
+    rot_mats = rodrigues_layer.batch_rodrigues(axisang)
+    print(rot_mats)
+    print("############")
+    r = R.from_euler('xyz', axisang)
+    print("as matrix:", r.as_matrix())
+    #print("rodrigues:", r.as_mrp())
+    # rot_mats = rodrigues_layer.batch_rodrigues(axisang)
+    # print(rot_mats)
+    # #print("angle", angle)
+
+test_scipy_rodrigues_5()
