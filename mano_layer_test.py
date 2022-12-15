fa79bb2e9098ef4c9ea4ea656670e83a8ff26a5d
MURAKI Taichi <muraki.taichi@gmail.com>
2022-12-15 11:14:34 +0900

    f

--- mano_layer_test.py
+++ mano_layer_test.py
@@ -61,36 +61,8 @@ def build_hand_dataset(yaml_file, args, is_train=True, scale_factor=1):
     return HandMeshTSVYamlDataset(args, yaml_file, is_train, False, scale_factor)
 
 
-# def make_hand_data_loader(args, yaml_file, is_distributed=True, is_train=True, start_iter=0, scale_factor=1):
-#     dataset = build_hand_dataset(yaml_file, args, is_train=is_train, scale_factor=scale_factor)
-#     logger = logging.getLogger(__name__)
-#     if is_train == True:
-#         shuffle = True
-#         images_per_gpu = args.per_gpu_train_batch_size
-#         images_per_batch = images_per_gpu * get_world_size()
-#         iters_per_batch = len(dataset) // images_per_batch
-#         num_iters = iters_per_batch * args.num_train_epochs
-#         logger.info("Train with {} images per GPU.".format(images_per_gpu))
-#         logger.info("Total batch size {}".format(images_per_batch))
-#         logger.info("Total training steps {}".format(num_iters))
-#     else:
-#         shuffle = False
-#         images_per_gpu = args.per_gpu_eval_batch_size
-#         num_iters = None
-#         start_iter = 0
-
-#     sampler = make_data_sampler(dataset, shuffle, is_distributed)
-#     batch_sampler = make_batch_data_sampler(sampler, images_per_gpu, num_iters, start_iter)
-#     data_loader = torch.utils.data.DataLoader(
-#         dataset,
-#         num_workers=args.num_workers,
-#         batch_sampler=batch_sampler,
-#         pin_memory=True,
-#     )
-#     return data_loader
-
 MetaInfo = namedtuple("MetaInfo", "mano_pose,trans,betas,joints_2d,joints_3d")
-Stats = namedtuple("Stats", "mean,std,var")
+
 
 
 def iter_meta_info(dataset_partial):
