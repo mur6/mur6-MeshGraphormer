26d74d6aad9fd4e55d13735c99dcbb4889cbce6e
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-10 15:38:59 +0900

    fix

--- add_mesh_info.py
+++ add_mesh_info.py
@@ -4,11 +4,11 @@ import os.path
 from collections import namedtuple
 from pathlib import Path
 
+import trimesh
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from PIL import Image
-
 
 import src.modeling.data.config as cfg
 from manopth.manolayer import ManoLayer
@@ -93,7 +93,7 @@ def adjust_vertices(gt_vertices, gt_3d_joints):
     # gt_3d_joints = gt_3d_joints - gt_3d_root[:, None, :]
     # gt_3d_joints_with_tag = torch.ones((batch_size, gt_3d_joints.shape[1], 4))
     # gt_3d_joints_with_tag[:, :, :3] = gt_3d_joints
-    return gt_vertices, gt_vertices_sub
+    return gt_vertices.squeeze(0), gt_vertices_sub.squeeze(0)
 
 
 def main(args, *, train_yaml_file, num):
@@ -114,13 +114,22 @@ def main(args, *, train_yaml_file, num):
     gt_vertices, gt_3d_joints = mano_model.layer(pose, betas)
     gt_vertices, gt_vertices_sub = adjust_vertices(gt_vertices, gt_3d_joints)
 
-    print(f"gt_vertices:{gt_vertices.shape}")
-    print(f"gt_vertices_sub:{gt_vertices_sub.shape}")
+    print(f"gt_vertices: {gt_vertices.shape}")
+    print(f"gt_vertices_sub: {gt_vertices_sub.shape}")
 
     # print(f"gt_vertices:min{torch.min(gt_vertices)}, max={torch.max(gt_vertices)}")
     # print(f"gt_3d_joints:min{torch.min(gt_3d_joints)}, max={torch.max(gt_3d_joints)}")
-    # print("gt_vertices", gt_vertices)
+    print("gt_vertices", gt_vertices)
+    mano_faces = mano_model.layer.th_faces
+    print(f"mano_faces: {mano_faces.shape}")
     # visualize_data_simple_scatter(ori_img.numpy().transpose(1, 2, 0), joints_2d, orig_joints_2d, orig_3d_joints)
+
+
+    # mesh objects can be created from existing faces and vertex data
+    mesh = trimesh.Trimesh(
+        vertices=gt_vertices,
+        faces=mano_faces)
+    mesh.show()
 
 
 
