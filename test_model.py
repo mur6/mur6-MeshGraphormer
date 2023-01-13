786142daa9ae6a67c4186d7678e43817f02e0088
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-13 15:41:18 +0900

    fix

--- test_model.py
+++ test_model.py
@@ -10,6 +10,53 @@ from sklearn.model_selection import train_test_split
 # from models import SpatialDescriptor, StructuralDescriptor, MeshConvolution
 from torch.utils.data import TensorDataset, DataLoader
 
+
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.utils.data
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+
+
+class STN3d(nn.Module):
+    def __init__(self):
+        super(STN3d, self).__init__()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
+
+
 class SpatialDescriptor(nn.Module):
 
     def __init__(self):
@@ -201,7 +248,13 @@ if __name__ == "__main__":
     # output = m(input)
     # print(output)
     with np.load("hand_infos.npz", allow_pickle=True) as hand_infos:
-        for v in hand_infos["arr_0"]:
-            print(v.keys())
-            perimeter = v['perimeter']
-            print(v['gt_vertices'].shape, " ", perimeter)
+        for val in hand_infos["arr_0"]:
+            perimeter = val['perimeter']
+            gt_vertices = torch.from_numpy(val['gt_vertices'])
+            betas = torch.from_numpy(val['betas'])
+            pose = torch.from_numpy(val['pose'])
+            center_points = torch.from_numpy(val['center_points'])
+            print(gt_vertices.shape)
+            print(betas.shape)
+            print(pose.shape)
+            print(center_points)
