0983c3be8913aa00f6ef6ec4e6ef544a8d459dea
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-18 16:05:46 +0900

    f

--- test_model_3d.py
+++ test_model_3d.py
@@ -64,13 +64,13 @@ def all_loss(y, y_pred, x, mano_faces):
 
 def exec_train(train_loader, test_loader, *, model, train_datasize, test_datasize, device, mano_faces, epochs=1000):
     #optimizer = optim.RMSprop(net.parameters(), lr=0.01)
-    if True:
+    if False:
         # optimizer = optim.SGD(model.parameters(), lr=0.01)
         optimizer = optim.AdamW(model.parameters(), lr=0.008)
         scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.90)
-    if False:
-        optimizer = optim.AdamW(model.parameters(), lr=0.005)
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30, eta_min=0.001)
+    if True:
+        optimizer = optim.AdamW(model.parameters(), lr=0.01)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20, eta_min=0.0002)
     E = nn.MSELoss()
     # トレーニング
     for epoch in range(epochs):
