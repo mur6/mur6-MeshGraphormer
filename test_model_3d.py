bdf901634cf9611aca3ae4e63a933902e97606c2
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-18 14:42:44 +0900

    fix

--- test_model_3d.py
+++ test_model_3d.py
@@ -54,10 +54,11 @@ def plane_loss(vert_3d, pca_mean, pca_components):
 
 def exec_train(train_loader, test_loader, *, model, train_datasize, test_datasize, device, epochs=1000):
     #optimizer = optim.RMSprop(net.parameters(), lr=0.01)
-    #optimizer = optim.AdamW(model.parameters(), lr=0.005)
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
-    # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30, eta_min=0.001)
+    if False:
+        optimizer = optim.SGD(model.parameters(), lr=0.01)
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+    optimizer = optim.AdamW(model.parameters(), lr=0.005)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30, eta_min=0.001)
     E = nn.MSELoss()
     # トレーニング
     for epoch in range(epochs):
