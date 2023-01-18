c9c9432a26636a04ed6f016d105a6b6ba3537ed6
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-18 10:56:21 +0900

    fix

--- src/handinfo/data.py
+++ src/handinfo/data.py
@@ -1,4 +1,5 @@
 import operator
+import math
 
 import numpy as np
 import torch
@@ -8,18 +9,21 @@ from sklearn.model_selection import train_test_split
 
 def _iter(*, dict_list, key_name="vert_3d"):
     for val in dict_list:
-        # perimeter = val['perimeter']
+        perimeter = val['perimeter']
         gt_vertices = val['gt_vertices']
         # betas = torch.from_numpy(val['betas'])
         # pose = torch.from_numpy(val['pose'])
         vert_3d = val['vert_3d']
         pca_mean = val['pca_mean_']
         pca_components = val['pca_components_']
+
+        normal_v = np.cross(pca_components[0], pca_components[1])
+
         if vert_3d.shape != (20, 3):
             # print(y.shape)
             vert_3d = vert_3d[:20, :]
         assert vert_3d.shape == (20, 3)
-        yield gt_vertices, vert_3d, pca_mean, pca_components
+        yield gt_vertices, vert_3d, pca_mean, pca_components, normal_v, perimeter
 
 
 def load_data(filename):
@@ -42,12 +46,28 @@ def load_data(filename):
 
     pca_mean = conv_float_tensor(2)
     pca_components = conv_float_tensor(3)
+    normal_v = conv_float_tensor(4)
+    perimeter = torch.FloatTensor([v[5].item(0) for v in values])
 
     (
         X_train, X_test, y_train, y_test,
         pca_mean_train, pca_mean_test,
-        pca_components_train, pca_components_test
-    ) = train_test_split(X, y, pca_mean, pca_components)
-    train_dataset = TensorDataset(X_train, y_train, pca_mean_train, pca_components_train)
-    test_dataset = TensorDataset(X_test, y_test, pca_mean_test, pca_components_test)
+        pca_components_train, pca_components_test,
+        normal_v_train, normal_v_test,
+        perimeter_train, perimeter_test,
+    ) = train_test_split(X, y, pca_mean, pca_components, normal_v, perimeter)
+    train_dataset = TensorDataset(
+        X_train, y_train,
+        pca_mean_train,
+        pca_components_train,
+        normal_v_train,
+        perimeter_train,
+    )
+    test_dataset = TensorDataset(
+        X_test, y_test,
+        pca_mean_test,
+        pca_components_test,
+        normal_v_test,
+        perimeter_test,
+    )
     return train_dataset, test_dataset
