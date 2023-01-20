f127612c03ea9decdd3e99e4aa38f87849e6cdf3
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-20 15:13:13 +0900

    ff

--- infer/infer_point_gnn.py
+++ infer/infer_point_gnn.py
@@ -8,10 +8,32 @@ from torch_geometric.datasets import ModelNet
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import MLP, PointConv, fps, global_max_pool, radius
 
-from src.model.geometric import GlobalSAModule, SAModule
+from src.model.geometric import GlobalSAModule, SAModule, Net
 
 # from src. pointnet2_classification import GlobalSAModule, SAModule
 
+
+def train(epoch):
+    model.train()
+
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        loss = F.nll_loss(model(data), data.y)
+        loss.backward()
+        optimizer.step()
+
+
+def test(loader):
+    model.eval()
+
+    correct = 0
+    for data in loader:
+        data = data.to(device)
+        with torch.no_grad():
+            pred = model(data).max(1)[1]
+        correct += pred.eq(data.y).sum().item()
+    return correct / len(loader.dataset)
 
 if __name__ == '__main__':
     path = osp.join(osp.dirname(osp.realpath(__file__)), '..',
@@ -19,16 +41,16 @@ if __name__ == '__main__':
     pre_transform, transform = T.NormalizeScale(), T.SamplePoints(1024)
     train_dataset = ModelNet(path, '10', True, transform, pre_transform)
     test_dataset = ModelNet(path, '10', False, transform, pre_transform)
-    # train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True,
-    #                           num_workers=6)
-    # test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False,
-    #                          num_workers=6)
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True,
+                              num_workers=6)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False,
+                             num_workers=6)
 
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # model = Net().to(device)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = Net().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    # for epoch in range(1, 201):
-    #     train(epoch)
-    #     test_acc = test(test_loader)
+    for epoch in range(1, 201):
+        train(epoch)
+        test_acc = test(test_loader)
     #     print(f'Epoch: {epoch:03d}, Test: {test_acc:.4f}')
