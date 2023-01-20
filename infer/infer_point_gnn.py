bf68cbc60526def029057c2fa55c681deaf14aa3
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-20 15:10:00 +0900

    f

--- infer/infer_point_gnn.py
+++ infer/infer_point_gnn.py
@@ -11,3 +11,24 @@ from torch_geometric.nn import MLP, PointConv, fps, global_max_pool, radius
 from src.model.geometric import GlobalSAModule, SAModule
 
 # from src. pointnet2_classification import GlobalSAModule, SAModule
+
+
+if __name__ == '__main__':
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..',
+                    'data/ModelNet10')
+    pre_transform, transform = T.NormalizeScale(), T.SamplePoints(1024)
+    train_dataset = ModelNet(path, '10', True, transform, pre_transform)
+    test_dataset = ModelNet(path, '10', False, transform, pre_transform)
+    # train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True,
+    #                           num_workers=6)
+    # test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False,
+    #                          num_workers=6)
+
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # model = Net().to(device)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # for epoch in range(1, 201):
+    #     train(epoch)
+    #     test_acc = test(test_loader)
+    #     print(f'Epoch: {epoch:03d}, Test: {test_acc:.4f}')
