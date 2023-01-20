fd4d3f1b9b5e547906a99b290e761ac92e53dc8a
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-20 17:53:59 +0900

    fix

--- src/model/geometric.py
+++ src/model/geometric.py
@@ -49,9 +49,10 @@ class Net(torch.nn.Module):
         self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
         self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
 
-        self.mlp = MLP([1024, 512, 256, 10], dropout=0.5, norm=None)
+        self.mlp = MLP([1024, 512, 256, 3], dropout=0.5, norm=None)
 
     def forward(self, data):
+        print(f"pos: {data.pos.shape}")
         sa0_out = (data.x, data.pos, data.batch)
         sa1_out = self.sa1_module(*sa0_out)
         sa2_out = self.sa2_module(*sa1_out)
