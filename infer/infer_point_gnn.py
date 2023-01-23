856fb9d31302caf9e5dc94085d4d262d594a8ced
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-23 10:54:10 +0900

    Fix

--- infer/infer_point_gnn.py
+++ infer/infer_point_gnn.py
@@ -1,4 +1,5 @@
-import os.path as osp
+from pathlib import Path
+# import os.path as osp
 
 import torch
 import torch.nn.functional as F
@@ -11,6 +12,19 @@ from torch_geometric.nn import MLP, PointConv, fps, global_max_pool, radius
 from src.model.geometric import GlobalSAModule, SAModule, Net
 from src.handinfo.data import load_data_for_geometric
 # from src. pointnet2_classification import GlobalSAModule, SAModule
+
+
+def save_checkpoint(model, epoch, iteration=None):
+    output_dir = Path("output")
+    output_dir.mkdir(exist_ok=True)
+    checkpoint_dir = output_dir / f"checkpoint-{epoch}" # -{iteration}
+    checkpoint_dir.mkdir(exist_ok=True)
+    model_to_save = model.module if hasattr(model, "module") else model
+
+    torch.save(model_to_save, checkpoint_dir / "model.bin")
+    torch.save(model_to_save.state_dict(), checkpoint_dir/ "state_dict.bin")
+    print(f"Save checkpoint to {checkpoint_dir}")
+    return checkpoint_dir
 
 
 def train(epoch, train_datasize):
@@ -59,8 +73,8 @@ if __name__ == '__main__':
     import sys
     filename = sys.argv[1]
     print(filename)
-    path = osp.join(osp.dirname(osp.realpath(__file__)), '..',
-                    'data/ModelNet10')
+    # path = osp.join(osp.dirname(osp.realpath(__file__)), '..',
+    #                 'data/ModelNet10')
     pre_transform, transform = T.NormalizeScale(), T.SamplePoints(1024)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
