dba29b687223fa50d016228350866307c3c9a10c
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-23 17:31:05 +0900

    fix

--- src/model/geometric.py
+++ src/model/geometric.py
@@ -42,27 +42,72 @@ class GlobalSAModule(torch.nn.Module):
         return x, pos, batch
 
 
+# class Net(torch.nn.Module):
+#     def __init__(self):
+#         super().__init__()
+
+#         # Input channels account for both `pos` and node features.
+#         self.sa1_module = SAModule(0.5, 0.2, MLP([3, 64, 64, 128]))
+#         self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
+#         self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
+
+#         self.mlp = MLP([1024, 512, 256, 3], dropout=0.5, norm=None)
+
+#     def forward(self, data):
+#         # print(f"pos: {data.pos.shape}")
+#         sa0_out = (data.x, data.pos, data.batch)
+#         sa1_out = self.sa1_module(*sa0_out)
+#         sa2_out = self.sa2_module(*sa1_out)
+#         sa3_out = self.sa3_module(*sa2_out)
+#         x, pos, batch = sa3_out
+#         # return self.mlp(x).log_softmax(dim=-1)
+#         return self.mlp(x)
+
+
+class FPModule(torch.nn.Module):
+    def __init__(self, k, nn):
+        super().__init__()
+        self.k = k
+        self.nn = nn
+
+    def forward(self, x, pos, batch, x_skip, pos_skip, batch_skip):
+        x = knn_interpolate(x, pos, pos_skip, batch, batch_skip, k=self.k)
+        if x_skip is not None:
+            x = torch.cat([x, x_skip], dim=1)
+        x = self.nn(x)
+        return x, pos_skip, batch_skip
+
+
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super().__init__()
 
         # Input channels account for both `pos` and node features.
-        self.sa1_module = SAModule(0.5, 0.2, MLP([3, 64, 64, 128]))
+        self.sa1_module = SAModule(0.2, 0.2, MLP([3 + 3, 64, 64, 128]))
         self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
         self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
 
-        self.mlp = MLP([1024, 512, 256, 3], dropout=0.5, norm=None)
+        self.fp3_module = FPModule(1, MLP([1024 + 256, 256, 256]))
+        self.fp2_module = FPModule(3, MLP([256 + 128, 256, 128]))
+        self.fp1_module = FPModule(3, MLP([128 + 3, 128, 128, 128]))
+
+        self.mlp = MLP([128, 128, 128, num_classes], dropout=0.5, norm=None)
+
+        self.lin1 = torch.nn.Linear(128, 128)
+        self.lin2 = torch.nn.Linear(128, 128)
+        self.lin3 = torch.nn.Linear(128, num_classes)
 
     def forward(self, data):
-        # print(f"pos: {data.pos.shape}")
         sa0_out = (data.x, data.pos, data.batch)
         sa1_out = self.sa1_module(*sa0_out)
         sa2_out = self.sa2_module(*sa1_out)
         sa3_out = self.sa3_module(*sa2_out)
-        x, pos, batch = sa3_out
-        # return self.mlp(x).log_softmax(dim=-1)
-        return self.mlp(x)
 
+        fp3_out = self.fp3_module(*sa3_out, *sa2_out)
+        fp2_out = self.fp2_module(*fp3_out, *sa1_out)
+        x, _, _ = self.fp1_module(*fp2_out, *sa0_out)
+
+        return self.mlp(x).log_softmax(dim=-1)
 
 if __name__ == "__main__":
     m = Net()
