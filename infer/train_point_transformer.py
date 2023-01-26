6b95ef8c296fcf07e97fd46c91bd2e613012fb90
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-26 16:57:14 +0900

    f

--- infer/train_point_transformer.py
+++ infer/train_point_transformer.py
@@ -157,7 +157,7 @@ def main(resume_dir, input_filename, batch_size, args):
             warmup_t=20,
             warmup_lr_init=5e-5,
             warmup_prefix=True)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
     gamma = float(args.gamma)
     print(f"gamma: {gamma}")
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=gamma)
