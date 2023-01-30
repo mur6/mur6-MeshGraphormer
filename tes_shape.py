c8db5c386511ebfc00266532679c32785614e449
MURAKI Taichi <muraki.taichi@gmail.com>
2023-01-30 15:03:00 +0900

    visualize test

--- tes_shape.py
+++ tes_shape.py
@@ -1,4 +1,9 @@
-import os.path as osp
+# import os.path as osp
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
 
 import torch
 import torch.nn.functional as F
@@ -12,7 +17,6 @@ from torch_geometric.nn import MLP, knn_interpolate
 from torch_geometric.utils import scatter
 from torch_geometric.data import Data, InMemoryDataset
 
-from pathlib import Path
 
 def main_1():
     category = 'Airplane'
@@ -106,11 +110,55 @@ def on_circle_loss(verts_3d, pred_pca_mean, pred_normal_v):
 # pred_output: torch.Size([32, 6])
 # pca_mean: torch.Size([32, 3])
 # normal_v: torch.Size([32, 3])
-def main():
+def new_loss_func_main():
     verts_3d = torch.randn(4, 20, 3)#: torch.Size([32, 20, 3])
     pred_output = torch.randn(4, 6)
     pca_mean = torch.randn(4, 3)
     normal_v = torch.randn(4, 3)
     loss = on_circle_loss(verts_3d, pca_mean, normal_v)
     print(loss)
+
+
+def calc_circle(a, b, radius):
+    theta = torch.linspace(-math.pi, math.pi, steps=100)
+    theta = theta.expand(3, -1).t()
+    # print(theta.shape)
+    z = a.unsqueeze(0) * torch.cos(theta) + b.unsqueeze(0) * torch.sin(theta)
+    return z * radius
+
+
+def main():
+    # pred: mean: tensor([-0.0285,  0.0415, -0.0629])
+    # gt: mean: tensor([-0.0247,  0.0446, -0.0663], dtype=torch.float64)
+    # pred: radius: tensor([0.0091])
+    # gt: radius: tensor([0.0082], dtype=torch.float64)
+    # pred: normal_v: tensor([ 1.0154e-03, -4.3981e-05, -2.5771e-03])
+    # gt:normal_v: tensor([ 0.4021, -0.2091, -0.8914], dtype=torch.float64)
+    mean = torch.tensor([-0.0247,  0.0446, -0.0663])
+    print(mean)
+
+    input = torch.tensor([[12., -51, 4], [0, 1, 0], [0, 0, 1]]).t()
+    # print(a)
+    q, r = torch.linalg.qr(input)
+    #print(r)
+    print(q[:, 0] * -52.545)
+    a = q[:, 1]
+    b = q[:, 2]
+    z = calc_circle(a, b, 0.09)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    print(z.shape)
+    for xs, ys, zs in z:
+        ax.scatter(xs, ys, zs, marker="o")
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+    # print(a, torch.linalg.vector_norm(a))
+    # print(b, torch.linalg.vector_norm(b))
+    # print(q[:, 0] @ q[:, 1])
+    # print(q[:, 0] @ q[:, 2])
+    # print(q[:, 1] @ q[:, 2])
 main()
