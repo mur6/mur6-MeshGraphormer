c93b9c72aac9bc04ffd247ee7df4e8b00465a740
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-01 17:42:47 +0900

    conv

--- src/model/pointnet.py
+++ src/model/pointnet.py
@@ -47,7 +47,7 @@ class Simple_STN3d(nn.Module):
 
 
 class STN3d(nn.Module):
-    in_features = 778
+    # in_features = 778
 
     def __init__(self):
         super(STN3d, self).__init__()
@@ -93,20 +93,16 @@ class STN3d(nn.Module):
 
 
 class PointNetfeat(nn.Module):
-    def __init__(self, global_feat = True):
+    def __init__(self):
         super(PointNetfeat, self).__init__()
         self.stn = STN3d()
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
-        self.output_feature_num = 20 * 3
+        self.output_feature_num = 7
         self.conv3 = torch.nn.Conv1d(128, self.output_feature_num, 1)
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(self.output_feature_num)
-        self.global_feat = global_feat
-        # self.feature_transform = feature_transform
-        # if self.feature_transform:
-        #     self.fstn = STNkd(k=64)
 
     def forward(self, x):
         n_pts = x.size()[2]
@@ -118,26 +114,9 @@ class PointNetfeat(nn.Module):
         x = torch.bmm(x, trans)
         x = x.transpose(2, 1)
         x = F.relu(self.bn1(self.conv1(x)))
-
-        # if self.feature_transform:
-        #     trans_feat = self.fstn(x)
-        #     x = x.transpose(2,1)
-        #     x = torch.bmm(x, trans_feat)
-        #     x = x.transpose(2,1)
-        # else:
-        #     trans_feat = None
-
-        # pointfeat = x
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.bn3(self.conv3(x))
         x = torch.max(x, 2, keepdim=True)[0]
-        # print(f"x: {x.shape}")
-        x = x.view(-1, 3, 20)
-        # if self.global_feat:
-        #     return x, trans, trans_feat
-        # else:
-        #     x = x.view(-1, 1024, 1).repeat(1, 1, n_pts)
-        #     return torch.cat([x, pointfeat], 1), trans,
         return x
 
 if __name__ == "__main__":
